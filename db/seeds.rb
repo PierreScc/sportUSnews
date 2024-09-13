fdb337749e0637dc10381880540880fbe05b8cff
Pierre Scouarnec <p.scouarnec1@gmail.com>
2024-09-13 18:30:25 +0200

    work

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+Article.create(title: "LeBron James marque 40 points", content: "LeBron a mené les Lakers à la victoire avec une performance incroyable...", league: "NBA")
+Article.create(title: "Tom Brady de retour", content: "Le quarterback des Buccaneers revient après une blessure...", league: "NFL")
+Article.create(title: "Les Yankees battent les Red Sox", content: "Les Yankees ont remporté une victoire historique hier soir...", league: "MLB")
